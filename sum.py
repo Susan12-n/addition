801dbecd0b268ac45b7b0d03ab00bb1bac99505e
Susan12-n <ngesasuzan@gmail.com>
2025-03-16 23:33:42 +0300

    calculator

---
+++ sum.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first num1: "))
+num2 = int(input("Enter first num2: "))
+sum = num1 + num2
+subtraction = num1 - num2
+product = num1 * num2
+division =num1 / num2
+print(sum)
+print(subtraction)
+print(product)
+print(division)
